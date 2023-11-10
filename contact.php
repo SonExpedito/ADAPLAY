a50ceceb934ff083231753f3f810f9715d88fc46
SonExpedito <alexexpeditoff7@gmail.com>
2023-11-09 23:43:53 -0300

    Outro leve ajuste na página de contato

--- contact.php
+++ contact.php
@@ -27,19 +27,7 @@ $p = new Cliente("127.0.0.1", "3308", "ecom_store", "root", "");
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-    
-
-    
-    
-    <title></title>
-</head>
-
-<body>
 
     <div class="container">
         <!--INSERIR MSG DO CLIENTE NO BANCO DE DADOS-->
